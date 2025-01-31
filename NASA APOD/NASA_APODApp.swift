28c8d4164be02eda5603e4e498b08534b92c80cf
longarinas <longarinas@AdminPrides-MacBook-Pro.local>
2025-01-31 18:17:43 -0300

    chore: first commit

---
+++ NASA APOD/NASA_APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_APODApp.swift
+//  NASA APOD
+//
+//  Created by longarinas on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NASA_APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
